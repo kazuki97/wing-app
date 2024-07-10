d77f2627eead6dc1c4980f2d3bb77b63f5cf9d75
kazuki97 <kazuki_09.07@icloud.com>
2024-07-10 22:09:38 +0900

    Update app.js

--- app.js
+++ app.js
@@ -505,7 +505,15 @@ document.addEventListener('DOMContentLoaded', function() {
     function showLoading() {
         loadingOverlay.classList.remove('hidden');
     }
-
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', function() {
+    navigator.serviceWorker.register('/service-worker.js').then(function(registration) {
+      console.log('ServiceWorker registration successful with scope: ', registration.scope);
+    }, function(err) {
+      console.log('ServiceWorker registration failed: ', err);
+    });
+  });
+}
     function hideLoading() {
         loadingOverlay.classList.add('hidden');
     }
