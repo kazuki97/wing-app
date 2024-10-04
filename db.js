bebc06fcc3a893811c6e4e65dbbc6cd1c44d83e0
kazuki97 <kazuki_09.07@icloud.com>
2024-10-04 17:11:13 +0900

    Update db.js

--- db.js
+++ db.js
@@ -1,12 +1,10 @@
 // db.js
-import { initializeApp } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-app.js";
-import { getFirestore } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-firestore.js";
+import { initializeApp } from 'https://www.gstatic.com/firebasejs/10.14.0/firebase-app.js';
+import { getFirestore } from 'https://www.gstatic.com/firebasejs/10.14.0/firebase-firestore.js';
+import { firebaseConfig } from './firebaseConfig.js';
 
-// Firebase の設定情報（firebaseConfig.js からインポート）
-import { firebaseConfig } from "./firebaseConfig.js";
-
-// Firebase アプリの初期化
+// Firebaseアプリの初期化
 const app = initializeApp(firebaseConfig);
 
-// Firestore データベースの取得
+// Firestoreデータベースの取得
 export const db = getFirestore(app);
