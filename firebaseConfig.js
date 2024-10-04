d998f8235a37cbbe06fb9923ec11eddeccf05212
kazuki97 <kazuki_09.07@icloud.com>
2024-10-04 17:24:06 +0900

    Delete firebaseConfig.js

--- firebaseConfig.js
+++
@@ -1,9 +0,0 @@
-const firebaseConfig = {
-    apiKey: "AIzaSyBND1iUtwW9-D3wisl5-wO_-Sfahab0zmA",
-    authDomain: "wing-system.firebaseapp.com",
-    projectId: "wing-system",
-    storageBucket: "wing-system.appspot.com",
-    messagingSenderId: "308022918518",
-    appId: "1:308022918518:web:ebdfe5deb82fa32a3f4e57",
-    measurementId: "G-806S4XB84E"
-  };
